fcaf401af465c7321d852ebce0077e64bc3dba98
Kareilys <kre.castcg@gmail.com>
2025-05-03 02:23:43 -0400

    Fix syntax error in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,12 @@
-import React from "react";\n\nconst Page = () => {\n  return (\n    <div>\n      <h1>Hello, World!</h1>\n    </div>\n  );\n};\n\nexport default Page;
+// app/page.tsx
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default Page;
